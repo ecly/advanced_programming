7aca3b77036b8978264e50c61cc2528257b22e63
Andrzej Wąsowski <wasowski@data.pl>
2018-02-14 20:01:49 +0100

    add missing declaration of t4

--- 030-option/Exercises.scala
+++ 030-option/Exercises.scala
@@ -145,6 +145,7 @@ object Tests extends App {
   // Exercise 3
   // assert (Tree.maximum (Branch(Leaf(1), Leaf(2))) == 2)
   // Exercise 4
+  // val t4 = Branch(Leaf(1), Branch(Branch(Leaf(2),Leaf(3)),Leaf(4)))
   // val t5 = Branch(Leaf("1"), Branch(Branch(Leaf("2"),Leaf("3")),Leaf("4")))
   // assert (Tree.map (t4) (_.toString) == t5)
 
