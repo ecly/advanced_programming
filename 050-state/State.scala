aeac01e2d3376d0e8c94dcd8c2fa0f344a6dc4a5
ecly <ecly@mailbox.org>
2018-03-05 18:08:15 +0100

    added first 6 exercises for week 5

--- 050-state/State.scala
+++ 050-state/State.scala
@@ -13,27 +13,52 @@ object RNG {
       (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
-
+  //
   // Exercise 1 (CB 6.1)
-
-  // def nonNegativeInt(rng: RNG): (Int, RNG) = { ...
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    rng.nextInt match {
+      case (Int.MinValue, next) => (Int.MaxValue, next)
+      case (value, next) => (Math.abs(value), next)
+    }
+  }
 
   // Exercise 2 (CB 6.2)
 
-  // def double(rng: RNG): (Double, RNG) = { ...
+  def double(rng: RNG): (Double, RNG) = {
+    rng.nextInt match {
+      case (value, next) => (Math.abs(value).toDouble/Int.MaxValue, next)
+    }
+  }
 
   // Exercise 3 (CB 6.3)
 
-  // def intDouble(rng: RNG): ((Int, Double), RNG) = { ...
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r1) = double(r)
+    ((i,d), r1)
+  }
 
-  // def doubleInt(rng: RNG): ((Double, Int), RNG) = { ...
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, r) = double(rng)
+    val (i, r1) = r.nextInt
+    ((d,i), r1)
+  }
 
   // def boolean(rng: RNG): (Boolean, RNG) =
   //  rng.nextInt match { case (i,rng2) => (i%2==0,rng2) }
 
   // Exercise 4 (CB 6.4)
 
-  // def ints(count: Int)(rng: RNG): (List[Int], RNG) =
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def f (c: Int)(r: RNG)(l: List[Int]): (List[Int], RNG) = {
+      if (c <= 0) (l, r)
+      else {
+        val (i, r1) = r.nextInt
+        f(c-1)(r1)(i::l)
+      }
+    }
+    f(count)(rng)(List())
+  }
 
   // There is something terribly repetitive about passing the RNG along
   // every time. What could we do to eliminate some of this duplication
@@ -55,12 +80,19 @@ object RNG {
   // def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
 
   // Exercise 5 (CB 6.5)
-
-  // val _double: Rand[Double] =
+  val _double: Rand[Double] =
+    map(nonNegativeInt)(i => Math.abs(i).toDouble/Int.MaxValue)
 
   // Exercise 6 (CB 6.6)
 
-  // def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, rng1) = ra(rng)
+      val (b, rng2) = rb(rng1)
+      (f(a,b), rng2)
+    }
+
+
 
   // this is given in the book
 
@@ -73,19 +105,13 @@ object RNG {
 
   // Exercise 7 (6.7)
 
-  //def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
-  //  fs.foldRight (unit(Nil):Rand[List[A]]) ((v, acc) => map2 (acc) (v) ((acc, v) => v::acc))
+  // def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
 
   // def _ints(count: Int): Rand[List[Int]] = ...
 
   // Exercise 8 (6.8)
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
-    rng => {
-      val (a, rng2) = f(rng)
-      g (a) (rng2)
-    }
-
+  // def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ...
 
   // def nonNegativeLessThan(n: Int): Rand[Int] = { ...
 
@@ -97,27 +123,11 @@ case class State[S, +A](run: S => (A, S)) {
 
   // Exercise 9 (6.10)
 
-  def map[B](f: A => B): State[S, B] =
-    State (
-      s => {
-        val (a, s2) = run (s)
-        (f (a), s2)
-      })
+  // def map[B](f: A => B): State[S, B] = ...
 
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    State (
-      s => {
-        val (a, s2) = run (s)
-        val (b, s3) = sb.run (s2)
-        (f (a, b), s3)
-      })
+  // def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ...
 
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    State(
-      s => {
-        val (a, s2) = run (s)
-        (f (a)).run(s2)
-      })
+  // def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => { ...
 
 }
 
@@ -129,9 +139,8 @@ object State {
 
   // Exercise 9 (6.10) continued
 
-  def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] =
-    sas.foldRight (unit(Nil):State[S, List[A]]) ((v, acc) => acc.map2 (v) ((acc, v) => v::acc))
-
+  // def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] = ...
+  //
   // This is given in the book:
 
   // def modify[S](f: S => S): State[S, Unit] = for {
@@ -148,16 +157,13 @@ object State {
 
   // Exercise 10
 
-  def state2stream[S,A] (s :State[S,A]) (seed :S) :Stream[A] = {
-    val (a, s2) = s.run(seed)
-    Stream.cons(a, state2stream (s) (s2))
-  }
+  // def state2stream[S,A] (s :State[S,A]) (seed :S) :Stream[A] = ...
 
   // Exercise 11
 
-  val random_integers = state2stream (random_int) (RNG.Simple(12313))
-  val ten_randoms = random_integers.take(10)
+  // val random_integers = ...
 
 }
 
 
+// vim:cc=80:foldmethod=indent:foldenable
