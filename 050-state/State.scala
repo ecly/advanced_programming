5cd83acf495db5a6f7060dc7786704af216ad5b7
Michael <miev@itu.dk>
2018-03-07 10:21:18 +0100

    Solve 7-11 in State (again)

--- 050-state/State.scala
+++ 050-state/State.scala
@@ -13,7 +13,7 @@ object RNG {
       (n, nextRNG) // The return value is a tuple containing both a pseudo-random integer and the next `RNG` state.
     }
   }
-  //
+
   // Exercise 1 (CB 6.1)
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     rng.nextInt match {
@@ -92,8 +92,6 @@ object RNG {
       (f(a,b), rng2)
     }
 
-
-
   // this is given in the book
 
   // def both[A,B](ra: Rand[A], rb: Rand[B]): Rand[(A,B)] =
@@ -104,31 +102,48 @@ object RNG {
   // val randDoubleInt: Rand[(Double, Int)] = both(double, int)
 
   // Exercise 7 (6.7)
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    fs.foldRight (unit(Nil):Rand[List[A]]) ((v, acc) => map2 (acc, v) ((acc, v) => v::acc))
 
-  // def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
-
-  // def _ints(count: Int): Rand[List[Int]] = ...
+  def _ints(count: Int): Rand[List[Int]] = sequence(List.fill(count)(int))
 
   // Exercise 8 (6.8)
 
-  // def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ...
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, rng2) = f(rng)
+      g (a) (rng2)
+    }
 
-  // def nonNegativeLessThan(n: Int): Rand[Int] = { ...
-
+  // Why not map?
+  def nonNegativeLessThan(n: Int): Rand[Int] = flatMap (nonNegativeInt) (v => rng => (v%n, rng))
 }
 
 import State._
 
 case class State[S, +A](run: S => (A, S)) {
-
   // Exercise 9 (6.10)
+  def map[B](f: A => B): State[S, B] =
+    State (
+      s => {
+        val (a, s2) = run (s)
+        (f (a), s2)
+      })
 
-  // def map[B](f: A => B): State[S, B] = ...
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
+    State (
+      s => {
+        val (a, s2) = run (s)
+        val (b, s3) = sb.run (s2)
+        (f (a, b), s3)
+      })
 
-  // def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = ...
-
-  // def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => { ...
-
+  def flatMap[B](f: A => State[S, B]): State[S, B] =
+    State(
+      s => {
+        val (a, s2) = run (s)
+        (f (a)).run(s2)
+      })
 }
 
 object State {
@@ -137,10 +152,13 @@ object State {
   def unit[S, A](a: A): State[S, A] =
     State(s => (a, s))
 
+
   // Exercise 9 (6.10) continued
 
-  // def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] = ...
-  //
+  def sequence[S,A](sas: List[State[S, A]]): State[S, List[A]] =
+    sas.foldRight (unit(Nil):State[S, List[A]]) ((v, acc) => acc.map2 (v) ((acc, v) => v::acc))
+
+
   // This is given in the book:
 
   // def modify[S](f: S => S): State[S, Unit] = for {
@@ -152,18 +170,17 @@ object State {
 
   def set[S](s: S): State[S, Unit] = State(_ => ((), s))
 
-
   def random_int :Rand[Int] =  State (_.nextInt)
 
-  // Exercise 10
-
-  // def state2stream[S,A] (s :State[S,A]) (seed :S) :Stream[A] = ...
+   // Exercise 10
+  def state2stream[S,A] (s :State[S,A]) (seed :S) :Stream[A] = {
+    val (a, s2) = s.run(seed)
+    Stream.cons(a, state2stream (s) (s2))
+  }
 
   // Exercise 11
-
-  // val random_integers = ...
-
+  val random_integers = state2stream (random_int) (RNG.Simple(12313))
+  val ten_randoms = random_integers.take(10)
 }
 
 
-// vim:cc=80:foldmethod=indent:foldenable
